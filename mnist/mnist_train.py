c14ab73d10c1c827b5c48a86540270a719231166
yucao <105818551+yucaoyc@users.noreply.github.com>
2023-05-23 17:36:06 +0800

    include mnist assets

---
+++ mnist/mnist_train.py
@@ -0,0 +1,177 @@
+import os
+import numpy as np
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import sys
+from lib.sdes import VariancePreservingSDE, PluginReverseSDE
+from lib.plotting import get_grid, plot_grids
+from lib.flows.elemwise import LogitTransform
+from lib.models.unet import UNet
+from lib.helpers import logging, create
+from tensorboardX import SummaryWriter
+import json
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+from torch.utils import collect_env
+import yaml
+import datetime
+
+retrain_model = False
+
+def run_simu(weight_type, seed, num_iterations, num_method="ei"):
+    torch.cuda.empty_cache()
+
+    with open('mnist.yaml', 'r') as file:
+        args = yaml.safe_load(file)
+    particular_str = "{:s}-{:s}-{:d}-{:d}".format(args['dataset'],weight_type,seed,num_iterations)
+
+    args['weight'] = weight_type
+    args['seed'] = seed
+    args['num_iterations'] = num_iterations
+    args['num_method'] = num_method
+
+    if args['weight'] == 'default':
+        w=lambda z : torch.ones_like(z)
+    elif args['weight'] == 'data':
+        w=lambda z : torch.ones_like(z)/(0.25 + z)
+    elif args['weight'] == 'noise':
+        w=lambda z : z
+
+    # training
+    torch.manual_seed(seed)
+    num_workers = args['num_workers']
+    input_channels = args['input_channels']
+    input_height = args['input_height']
+    dimx = input_channels * input_height ** 2
+
+    transform = transforms.Compose([transforms.ToTensor()])
+    trainset = torchvision.datasets.MNIST(root=args['dataroot'], train=True,
+                                          download=True, transform=transform)
+    testset = torchvision.datasets.MNIST(root=args['dataroot'], train=False,
+                                         download=True, transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=args['batch_size'],
+                                              shuffle=True, num_workers=num_workers)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=args['test_batch_size'],
+                                             shuffle=True, num_workers=num_workers)
+    drift_q = UNet(
+        input_channels=input_channels,
+        input_height=input_height,
+        ch=32,
+        #ch_mult=(1, 2, 2),
+        ch_mult=(1,2,),
+        num_res_blocks=2,
+        attn_resolutions=(16,),
+        resamp_with_conv=True,
+    )
+    _folder_name_keys = ['dataset', 'weight', 'real', 'debias', \
+                         'batch_size', 'lr', 'num_iterations', 'T0', 'seed', 'num_method']
+    folder_name = '-'.join([str(args[k]) for k in _folder_name_keys])
+    create(args['saveroot'], folder_name)
+
+    folder_path = os.path.join(args['saveroot'], folder_name)
+    print_ = lambda s: logging(s, folder_path)
+    print_(f'folder path: {folder_path}')
+    with open(os.path.join(folder_path, 'args.txt'), 'w') as out:
+        out.write(json.dumps(args, indent=4))
+    with open(os.path.join(folder_path, 'env.txt'), 'w') as out:
+        out.write(collect_env.get_pretty_env_info())
+    writer = SummaryWriter(folder_path)
+
+    T = torch.nn.Parameter(torch.FloatTensor([args['T0']]), requires_grad=False)
+    inf_sde = VariancePreservingSDE(beta_min=0.1, beta_max=20.0, T=T)
+    gen_sde = PluginReverseSDE(inf_sde, drift_q, T, vtype=args['vtype'], debias=args['debias'])
+
+    total_params = sum(p.numel () for p in gen_sde.parameters ())
+    print("Total number of parameters = {:d}".format(total_params))
+
+    if torch.cuda.is_available():
+        gen_sde.cuda()
+
+    optim = torch.optim.Adam(gen_sde.parameters(), lr=args['lr'])
+
+    logit = LogitTransform(alpha=args["logit_alpha"])
+    if args['real']:
+        reverse = logit.reverse
+    else:
+        reverse = None
+
+    ###############################################################################
+    # Training
+    loss_vec = np.zeros(args['num_iterations'])
+    torch.manual_seed(seed);
+    if os.path.exists(os.path.join(folder_path, 'checkpoint.pt')) and (retrain_model==False):
+        gen_sde, optim, not_finished, count, loss_vec = torch.load(os.path.join(folder_path, 'checkpoint.pt'))
+        if not_finished == False:
+            print("The model is fully trained and we simply load the checkpoint!")
+    else:
+        not_finished = True
+        count = 0
+        writer.add_scalar('T', gen_sde.T.item(), count)
+
+    while not_finished:
+        for x, _ in trainloader:
+            if torch.cuda.is_available():
+                x = x.cuda()
+            x = x * 255 / 256 + torch.rand_like(x) / 256
+            if args['real']:
+                x, _ = logit.forward_transform(x, 0)
+
+            loss = gen_sde.dsm(x, w=w).mean()
+
+            optim.zero_grad()
+            loss.backward()
+            optim.step()
+
+            loss_vec[count] = loss.item()
+
+            count += 1
+            if count == 1 or count % args['print_every'] == 0:
+                writer.add_scalar('loss', loss.item(), count)
+                writer.add_scalar('T', gen_sde.T.item(), count)
+                print("{:s}\t Iteration={:9d}\t Loss={:.4f}".format( \
+                        str(datetime.datetime.now()), count, loss.item()))
+
+            if count >= args['num_iterations']:
+                not_finished = False
+                print_('Finished training')
+                break
+
+            if count % args['sample_every'] == 0:
+                gen_sde.eval()
+                # do something...
+                gen_sde.train()
+
+            if count % args['checkpoint_every'] == 0:
+                torch.save([gen_sde, optim, not_finished, count, loss_vec], \
+                           os.path.join(folder_path, 'checkpoint.pt'))
+
+    if not_finished == False:
+        torch.save([gen_sde, optim, not_finished, count, loss_vec], os.path.join(folder_path, 'checkpoint.pt'))
+
+    plt.figure(figsize=(4,3))
+    plt.plot(np.arange(0, stop=args['num_iterations']), loss_vec)
+    plt.title("Loss")
+    plt.yscale("log")
+    plt.savefig(folder_path + "/loss_{:s}.eps".format(particular_str))
+    plt.savefig(folder_path + "/loss_{:s}.pdf".format(particular_str))
+
+
+    ###############################################################################
+    # plot 10 * 10 figures
+    print("start to generate samples")
+    torch.manual_seed(seed);
+    create(folder_path, "assets")
+    num_steps = 100
+    for c in tqdm(np.sqrt(np.array([0.0, 0.5, 1.0, 1.5, 2.0, 2.5]))):
+        torch.manual_seed(seed)
+        fig, gen_img, gen_img_data = plot_grids(gen_sde, c, logit, args, num_method=num_method, \
+                                                num_steps=num_steps, n=10, s=8, binary=True)
+        plt.savefig(folder_path + ("/assets/img_{:s}_{:.2f}".format(particular_str,c)).replace('.','-') + ".eps")
+        plt.savefig(folder_path + ("/assets/img_{:s}_{:.2f}".format(particular_str,c)).replace('.','-') + ".pdf")
+        torch.save((gen_img, gen_img_data), folder_path + "/assets/{:s}_img_data_{:.2f}.pt".format(args['dataset'],c))
+
+for weight_type in ["default","data","noise"]:
+    for seed in [1, 2]:
+        for iter_num in [20000]:
+            run_simu(weight_type, seed, iter_num, num_method="ei")
---
+++ mnist/mnist_visualize.py
@@ -0,0 +1,161 @@
+import os
+import numpy as np
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import sys
+from lib.sdes import VariancePreservingSDE, PluginReverseSDE
+from lib.plotting import get_grid, plot_grids
+from lib.flows.elemwise import LogitTransform
+from lib.models.unet import UNet
+from lib.helpers import logging, create
+from tensorboardX import SummaryWriter
+import json
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+from torch.utils import collect_env
+import yaml
+import datetime
+
+def load_model(weight_type, seed, num_iterations, num_method):
+    torch.cuda.empty_cache()
+    with open('mnist.yaml', 'r') as file:
+        args = yaml.safe_load(file)
+    particular_str = "{:s}-{:s}-{:d}-{:d}".format(args['dataset'],weight_type,seed,num_iterations)
+
+    args['weight'] = weight_type
+    args['seed'] = seed
+    args['num_iterations'] = num_iterations
+    args['num_method'] = num_method
+
+    # training
+    torch.manual_seed(seed)
+    num_workers = args['num_workers']
+    input_channels = args['input_channels']
+    input_height = args['input_height']
+    dimx = input_channels * input_height ** 2
+
+    transform = transforms.Compose([transforms.ToTensor()])
+    trainset = torchvision.datasets.MNIST(root=args['dataroot'], train=True,
+                                          download=True, transform=transform)
+    testset = torchvision.datasets.MNIST(root=args['dataroot'], train=False,
+                                         download=True, transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=args['batch_size'],
+                                              shuffle=True, num_workers=num_workers)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=args['test_batch_size'],
+                                             shuffle=True, num_workers=num_workers)
+    drift_q = UNet(
+        input_channels=input_channels,
+        input_height=input_height,
+        ch=32,
+        ch_mult=(1,2,),
+        num_res_blocks=2,
+        attn_resolutions=(16,),
+        resamp_with_conv=True,
+    )
+    _folder_name_keys = ['dataset', 'weight', 'real', 'debias', \
+                         'batch_size', 'lr', 'num_iterations', 'T0', 'seed', 'num_method']
+    folder_name = '-'.join([str(args[k]) for k in _folder_name_keys])
+    create(args['saveroot'], folder_name)
+
+    folder_path = os.path.join(args['saveroot'], folder_name)
+ 
+    T = torch.nn.Parameter(torch.FloatTensor([args['T0']]), requires_grad=False)
+    inf_sde = VariancePreservingSDE(beta_min=0.1, beta_max=20.0, T=T)
+    gen_sde = PluginReverseSDE(inf_sde, drift_q, T, vtype=args['vtype'], debias=args['debias'])
+
+    if torch.cuda.is_available():
+        gen_sde.cuda()
+
+    logit = LogitTransform(alpha=args["logit_alpha"])
+
+    if args['real']:
+        reverse = logit.reverse
+    else:
+        reverse = None
+
+    torch.manual_seed(seed);
+ 
+    if os.path.exists(os.path.join(folder_path, 'checkpoint.pt')):
+        gen_sde, _, _, _, _ = torch.load(os.path.join(folder_path, 'checkpoint.pt'))
+        return gen_sde, logit, args
+    else:
+        print("File Not Found!")
+        return None
+
+#############################################
+# Parameters and setup
+weight_type_list = ['data','default','noise']
+seed_list = [1, 2]
+num_steps_list = [30, 60, 100, 200, 1000]
+iter_num = 20000
+num_method = 'ei'
+c_vec = np.sqrt(np.array([0.0, 0.5, 1.0, 1.5, 2.0, 2.5]))
+n = 10
+data = torch.zeros(len(weight_type_list), len(seed_list), len(num_steps_list), len(c_vec), 28*n, 28*n)
+
+
+#############################################
+# get all data
+for i in range(len(weight_type_list)):
+    for j in range(len(seed_list)):
+        for l in range(len(num_steps_list)):
+            print((i,j,l))
+            for c_idx in tqdm(range(len(c_vec))):
+                weight_type = weight_type_list[i]
+                seed = seed_list[j]
+                num_steps = num_steps_list[l]
+                c = c_vec[c_idx]
+                torch.manual_seed(seed)
+
+                gen_sde, logit, args = load_model(weight_type, seed, iter_num, num_method)
+
+                _, gen_img, _ = plot_grids(gen_sde, c, logit, args, num_method=num_method, \
+                                            num_steps=num_steps, n=n, binary=True)
+                data[i,j,l,c_idx,:,:] = torch.tensor(gen_img[0,:,:])
+                plt.close()
+# save data
+torch.save(data, "img_data.pt")
+
+#############################################
+# Effect of weight
+c_idx = 0 # ODE
+num_step_idx = 4
+
+print("{:2.1f}".format(c_vec[c_idx]**2))
+print(num_steps_list[num_step_idx])
+
+for seed_idx in [0, 1]:
+    plt.figure(figsize=(9,3))
+    for i in range(len(weight_type_list)):
+        plt.subplot(1,3,i+1)
+        plt.imshow(data[i,seed_idx,num_step_idx, c_idx,:,:], cmap='gray')
+        plt.title('weight type = ' + weight_type_list[i], fontsize=14)
+        plt.xticks([]); plt.yticks([])
+    plt.tight_layout()
+    plt.savefig("../assets/MNIST_ODE_with_different_training" \
+                + "{:d}-{:d}".format(seed_list[seed_idx], num_steps_list[num_step_idx]) \
+                + ".eps")
+
+#############################################
+# Effect of weight & discretization steps
+for weight_type_idx in [0, 1, 2]: #'default'
+    for seed_idx in [0, 1]:
+        num_row = 5
+        num_col = 5
+        plt.figure(figsize=(10,10))
+        for k in range(num_row): # num step
+            for i in range(num_col): # c
+                plt.subplot(num_row, num_col, k*num_col + i + 1)
+                plt.imshow(data[weight_type_idx, seed_idx, k, i, :, :], cmap='gray')
+                if k == 0:
+                    plt.title(r'$h^2/g^2 = {:2.1f}$'.format(c_vec[i]**2), fontsize=14)
+                if i == 0:
+                    plt.ylabel("step={:d}".format(num_steps_list[k]), fontsize=14)
+                plt.xticks([]); plt.yticks([])
+        plt.suptitle("weight type={:s}, trial={:d}".format(weight_type_list[weight_type_idx], \
+                                                          seed_list[seed_idx]), fontsize=16)
+        plt.tight_layout()
+        plt.savefig("../assets/MNIST_effect_of_c_" + \
+                    "{:s}_{:d}".format(weight_type_list[weight_type_idx], \
+                    seed_list[seed_idx]) + ".eps")
